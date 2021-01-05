54b51a7826f51b79a0c0f95c0c20b8d60950512f
rejane-b <rejaneofb@gmail.com>
2021-01-04 22:45:30 -0500

    Initial commit

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,44 @@
+// from data.js
+var tableData = data;
+
+console.log("tableData: ",tableData);
+
+
+
+var tbody = d3.select("tbody");
+
+function constructTable(tableData){
+
+  
+  tbody.html("");
+
+  tableData.forEach(function(element){
+    var tr = tbody.append("tr");
+    tr.append("td").attr("class","ufosight").text(element.datetime);
+    tr.append("td").attr("class","ufosight").text(element.city);
+    tr.append("td").attr("class","ufosight").text(element.state);
+    tr.append("td").attr("class","ufosight").text(element.country);
+    tr.append("td").attr("class","ufosight").text(element.shape);
+    tr.append("td").attr("class","ufosight").text(element.durationMinutes);
+    tr.append("td").attr("class","ufosight").text(element.comments);
+  });
+}
+
+function searchThroughDateTime(){
+  console.log("Inside searchThroughDateTime");
+
+  
+  d3.event.preventDefault();
+  var date = d3.select("#datetime").property("value");
+  console.log("date seleected : ", date);
+  let filterUFOSightData = tableData;
+  if(date){
+    filterUFOSightData = filterUFOSightData.filter(row => row.datetime === date);
+  console.log("filterUFOSightData:", filterUFOSightData);
+}
+  constructTable(filterUFOSightData);
+}
+
+d3.selectAll("#filter-btn").on("click", searchThroughDateTime);
+
+constructTable(tableData);
